6de1d39384621530b9007e821c24a6b64ad4a1f4
Flavien <saintsaens@users.noreply.github.com>
2024-03-18 09:53:09 +0100

    Create zdesk-article-edit-mode.js

---
+++ zdesk-article-edit-mode.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Zendesk article edit mode
+// @version      1.0
+// @description  Hit e to open a Zendesk article in edit mode.
+// @author       Flavien
+// @include      https://support.360learning.com/hc/*
+// ==/UserScript==
+
+
+function clickNext() {
+    // Run .click() only if element is not undefined.
+    document.querySelector(".zd-hc-button")?.click();
+}
+
+// Add the event listener.
+function init() {
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'e') {
+            clickNext();
+        }
+    })
+}
+
+init();
