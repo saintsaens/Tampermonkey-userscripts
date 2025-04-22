45145d60d10c31e32f951507d10f9146107b22e6
Flavien <saintsaens@users.noreply.github.com>
2025-04-22 11:34:07 +0200

    Update zendesk-article-fr-en-toggler.user.js

--- zendesk-article-fr-en-toggler.user.js
+++ zendesk-article-fr-en-toggler.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Zendesk article French-English toggler
-// @version      1.0
+// @version      1.1
 // @description  Hit f to switch between the French and English version of a Zendesk article in view mode.
 // @author       Flavien
-// @include      https://support.360learning.com/hc/*
+// @include      https://support.360learning.com/hc/en-us/articles/*
+// @include      https://support.360learning.com/hc/fr/articles/*
 // ==/UserScript==
-
 
 function transformAndReload() {
     const currentURL = window.location.href;
@@ -25,13 +25,24 @@ function transformURL(url) {
     }
 }
 
+function isSearchFocused() {
+    const searchForm = document.querySelector("form[role='search']");
+    return searchForm?.classList.contains("is-focused");
+}
+
 // Add the event listener.
 function init() {
     document.addEventListener('keydown', (event) => {
+        // Prevent action if the find function is triggered (cmd/ctrl + f) or search form is focused
+        if ((event.key === 'f' && (event.metaKey || event.ctrlKey)) || isSearchFocused()) {
+            return;
+        }
+
+        // Trigger the switch only when the 'f' key is pressed without cmd/ctrl
         if (event.key === 'f') {
             transformAndReload();
         }
-    })
+    });
 }
 
 init();
